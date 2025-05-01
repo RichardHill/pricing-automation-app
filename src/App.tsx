68beb213dca974c98aaad83191fa9d3db1e3c5f3
Richard Hill <richard.hill@bentley.com>
2025-05-01 22:13:36 +0100

    Styling

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,13 @@
 import React from "react";
+import "./App.css";
 import PricingForm from "./components/PricingForm";
 
 function App() {
   return (
-    <div>
-      <PricingForm />
+    <div className="app-container">
+      <div className="form-wrapper">
+        <PricingForm />
+      </div>
     </div>
   );
 }
